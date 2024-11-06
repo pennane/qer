18740adb0940d019dd2dd65115d6830c54c006f2
Arttu Pennanen <39066923+pennane@users.noreply.github.com>
2024-11-06 19:44:43 +0200

    post -> get

--- packages/server/src/routes/auth.ts
+++ packages/server/src/routes/auth.ts
@@ -12,7 +12,7 @@ authRouter.post('/accept', async (req, res) => {
 	res.send(200)
 })
 
-authRouter.post('/me', async (req, res) => {
+authRouter.get('/me', async (req, res) => {
 	const { profile } = await getSpotify(req)
 	if (!profile) {
 		res.status(401).json({ msg: 'moro, kirjaudu sisää' })
